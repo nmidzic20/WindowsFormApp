b649c0546cdab72daebf73bc72faf40549d9baf1
Noa Midzic <nmidzic@yahoo.com>
2022-10-21 16:25:46 +0200

    Implementirane klase poslovne logike. Implementirane klase User, UserType, UserRepository.

---
+++ Lab1_LoginRegistration/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_LoginRegistration.Models
+{
+    internal class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public UserType UserType { get; set; }
+
+    }
+}
---
+++ Lab1_LoginRegistration/Repositories/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Lab1_LoginRegistration.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_LoginRegistration.Repositories
+{
+    internal static class UserRepository
+    {
+        public static List<User> Users { get; set; }
+    }
+}
